128e419619be32caf1155ed40768370ec3d1710e
Val Sergeev <destriero@ya.ru>
2020-08-19 16:44:36 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://rekseo.ru/local/templates/ezco/images/partners/partner4.png" alt="logo"/>
+        </header>
+    );
+}
+
+export default Header;
